d44d50d948746a24ecdcb81c28a3794fafb6b86a
niks-yad <nikhilyadav1.ny@gmail.com>
2024-05-26 22:14:05 +0100

    commit

--- Upload/upload.py
+++ Upload/upload.py
@@ -4,9 +4,11 @@ from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
 
+# Directory where uploaded files will be stored
 UPLOAD_FOLDER = '/path/to/the/uploads'
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
+# Allowed file extensions
 ALLOWED_EXTENSIONS = {'mp4', 'avi', 'mov', 'mkv'}
 
 def allowed_file(filename):
@@ -15,9 +17,11 @@ def allowed_file(filename):
 
 @app.route('/upload', methods=['POST'])
 def upload_file():
+    # Check if the post request has the file part
     if 'file' not in request.files:
         return jsonify(message="No file part"), 400
     file = request.files['file']
+    # If the user does not select a file, the browser submits an empty part without filename
     if file.filename == '':
         return jsonify(message="No selected file"), 400
     if file and allowed_file(file.filename):
